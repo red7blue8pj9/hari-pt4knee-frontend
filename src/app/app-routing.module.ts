abcf47b185c1fcfc0efb957b73e2d0593058f654
jing <jip45@pitt.edu>
2020-06-04 10:51:25 -0400

    v1.2 add a new line chart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,25 +9,23 @@ import {VisualizationComponent} from './pages/visualization/visualization.compon
 
 
 const childRoutes: Routes = [
-  {path: '', redirectTo: 'datasets', pathMatch: 'full'},
-  {path: 'datasets', component: DatasetsComponent},
-  {path: 'visualization', component: VisualizationComponent}
+    {path: '', redirectTo: 'datasets', pathMatch: 'full'},
+    {path: 'datasets', component: DatasetsComponent},
+    {path: 'visualization', component: VisualizationComponent}
 ];
 
 const appRoutes: Routes = [
-  {path: '', redirectTo: 'main', pathMatch: 'full', canActivate: [AuthGuard]},
-  {path: 'login', component: LoginComponent},
-  {path: 'signup', component: SignupComponent},
-  {
-    path: 'main', component: MainComponent, children: childRoutes, canActivate: [AuthGuard]
-  },
-  {path: '**', redirectTo: ''}
+    {path: '', redirectTo: 'main', pathMatch: 'full', canActivate: [AuthGuard]},
+    {path: 'login', component: LoginComponent},
+    {path: 'signup', component: SignupComponent},
+    {path: 'main', component: MainComponent, children: childRoutes, canActivate: [AuthGuard]},
+    {path: '**', redirectTo: ''}
 ];
 
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes, {useHash: true})],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(appRoutes, {useHash: true})],
+    exports: [RouterModule]
 })
 export class AppRoutingModule {
 }
